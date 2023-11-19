b2fb2b2b33528ffe0a4bfd589eb81ca4c36b85b7
inhasid <sidorchuk.inga@gmail.com>
2023-11-19 19:20:51 +0100

    commit11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 
